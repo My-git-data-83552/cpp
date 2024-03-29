9386225d49a11627cfce8ae6cbe91344cffb1eae
kd2_83552_shantanukhilare <shantanu.khilare.16@gmail.com>
2024-03-29 21:27:53 +0530

    Assignement 8 & 9 uploaded

---
+++ Assignment8/assignment8.cpp
@@ -0,0 +1,185 @@
+#include<iostream>
+using namespace std;
+
+enum Eaccounttype {
+    SAVING = 1,
+    CURRENT,
+    DMAT
+};
+
+class InsufficientFundsException {
+private:
+    int accno;
+    double currentBalance;
+    double withdrawAmount;
+public:
+    void display() {
+        if (currentBalance < withdrawAmount) {
+            cout << "Insufficient Balance" << endl;
+        }
+        else if (withdrawAmount < 0) {
+            cout << "cannot withdraw value less than 0 " << endl;
+        }
+    }
+    InsufficientFundsException() {
+
+    }
+    InsufficientFundsException(int accno, double currentBalance, double withdrawAmount) {
+        this->accno = accno;
+        this->currentBalance = currentBalance;
+        this->withdrawAmount = withdrawAmount;
+
+    }
+
+};
+
+class Account {
+private:
+    const int accno;
+    Eaccounttype type;
+    double balance;
+    static int generateno;
+
+public:
+    Account() : accno(++generateno) {
+
+    }
+
+    Account(Eaccounttype type, double balance) : accno(++generateno) {
+        this->type = type;
+        this->balance = balance;
+    }
+
+    int getAccNo() const {
+        return accno;
+    }
+
+    void accept() {
+        int ch;
+        cout << "Enter Balance:" << endl;
+        cin >> balance;
+
+        cout << "Enter Account Type:\n1.Saving\n2.Current\n3.Dmat" << endl;
+        cin >> ch;
+        type = Eaccounttype(ch);
+    }
+
+    void display() {
+        cout << "-----Account Details-----" << endl;
+        cout << "Acc No:" << accno << endl;
+        cout << "AccountType:" << type << endl;
+        cout << "Balance:" << balance << endl;
+    }
+
+    void deposit(double amount) {
+        if (amount < 0)
+            throw 1;
+        else {
+            balance = amount + balance;
+        }
+    }
+
+    void withdraw(double amount) {
+        if (balance < amount) {
+            throw InsufficientFundsException(accno, balance, amount);
+        }
+        else if (amount < 0) {
+            throw InsufficientFundsException(accno, balance, amount);
+        }
+        else {
+            balance = balance - amount;
+        }
+    }
+};
+
+int Account::generateno = 100000;
+
+int main() {
+    int choice;
+    int index = 0;
+    Account* ptr[5];
+
+    do {
+        cout << "-------Welcome to Banking System-----------" << endl;
+        cout << "0.Exit" << endl;
+        cout << "1.Add Account details" << endl;
+        cout << "2.Display Account details" << endl;
+        cout << "3.Deposit money" << endl;
+        cout << "4.Withdraw money" << endl;
+        cout << "Enter your choice:" << endl;
+        cin >> choice;
+
+        switch (choice) {
+            case 0:
+                cout << "---------Thank you for using Bank--------------------" << endl;
+                break;
+
+            case 1:
+                if (index < 5) {
+                    ptr[index] = new Account();
+                    ptr[index]->accept();
+                    index++;
+                }
+                else {
+                    cout << "Array is full" << endl;
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < index; i++) {
+                    ptr[i]->display();
+                }
+                break;
+
+            case 3:
+                int depositAccNo;
+                double depositAmount;
+                cout << "Enter Account Number to deposit into:" << endl;
+                cin >> depositAccNo;
+                cout << "Enter Amount to deposit:" << endl;
+                cin >> depositAmount;
+                
+                for (int i = 0; i < index; i++) {
+                    if (ptr[i]->getAccNo() == depositAccNo) {
+                        try {
+                            ptr[i]->deposit(depositAmount);
+                        }
+                        catch (int) {
+                            cout << "Invalid Amount to deposit" << endl;
+                        }
+                        break;
+                    }
+                }
+                break;
+
+            case 4:
+                int withdrawAccNo;
+                double withdrawAmount;
+                cout << "Enter Account Number to withdraw from:" << endl;
+                cin >> withdrawAccNo;
+                cout << "Enter Amount to withdraw:" << endl;
+                cin >> withdrawAmount;
+                
+                for (int i = 0; i < index; i++) {
+                    if (ptr[i]->getAccNo() == withdrawAccNo) {
+                        try {
+                            ptr[i]->withdraw(withdrawAmount);
+                        }
+                        catch (InsufficientFundsException e) {
+                            e.display();
+                        }
+                        break;
+                    }
+                }
+                break;
+
+            default:
+                cout << "Invalid Choice!!!!!!!!!!" << endl;
+                break;
+        }
+    } while (choice != 0);
+
+    
+
+    return 0;
+}
